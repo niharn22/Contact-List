6a818d3aacd09e346a960c84efc2cb9906ddf904
Nihar Nandoskar <niharnandoskar@gmail.com>
2023-08-15 01:05:22 +0530

    first commit

---
+++ lists.py
@@ -0,0 +1,13 @@
+myList = ["banana","cherry","apple"]
+print(myList)
+
+if "banana" in myList:
+    print("yes")
+else:
+    print("No")
+
+print(len(myList))
+
+myList.append("lemon")
+print(myList)
+
